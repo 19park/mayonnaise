60876e5a625cb5cf8816aee7faa108c2ba75fa95
19park <19park@wholedoc.net>
2018-08-02 13:20:18 +0900

    thumnail list

--- components/NavigationDrawer.vue
+++ components/NavigationDrawer.vue
@@ -2,15 +2,19 @@
   <div class="mdl-layout__drawer">
     <span class="mdl-typography--text-center">
       <img style="height:100%; width:100%;" src="~/assets/img/iweee-logo-square.png" />
-      <h1 class="mdl-typography--display-1 title"><span style="display:none;">마요와네즈</span></h1>
+      <h1 class="mdl-typography--display-1 title">
+        <span style="display:none;">마요와네즈</span>
+      </h1>
     </span>
     <nav class="mdl-navigation">
       <nuxt-link to="/" class="mdl-navigation__link typcn typcn-home iweee-navigation__link--home" @click.native="closeDrawer">메인</nuxt-link>
-      <nuxt-link to="/about" class="mdl-navigation__link typcn typcn-info-large iweee-navigation__link--about" @click.native="closeDrawer">About</nuxt-link>
+      <nuxt-link to="/gallery" class="mdl-navigation__link typcn typcn-map iweee-navigation__link--directions" @click.native="closeDrawer">갤러리</nuxt-link>
+      <!--<nuxt-link to="/about" class="mdl-navigation__link typcn typcn-info-large iweee-navigation__link--about" @click.native="closeDrawer">About</nuxt-link>
       <nuxt-link to="/program-schedules" class="mdl-navigation__link typcn typcn-document-text iweee-navigation__link--program-schedules" @click.native="closeDrawer">Program Schedules</nuxt-link>
       <nuxt-link to="/important-dates" class="mdl-navigation__link typcn typcn-calendar iweee-navigation__link--important-dates" @click.native="closeDrawer">Important Dates</nuxt-link>
       <nuxt-link to="/directions" class="mdl-navigation__link typcn typcn-map iweee-navigation__link--directions" @click.native="closeDrawer">Directions</nuxt-link>
       <nuxt-link to="/abstract-submission" class="mdl-navigation__link typcn typcn-upload iweee-navigation__link--abstract-submission" @click.native="closeDrawer">Abstract Submission</nuxt-link>
+      //-->
     </nav>
   </div>
 </template>
@@ -42,7 +46,8 @@ export default {
   margin: 0;
 }
 
-.title, .mdl-navigation {
+.title,
+.mdl-navigation {
   border-right: 1px solid #e0e0e0;
 }
 
