029770746db68ea2a94eb7303d1eb06c6cd86faa
19park <19park@wholedoc.net>
2018-08-02 11:34:32 +0900

    about page 수정

--- pages/about.vue
+++ pages/about.vue
@@ -3,78 +3,105 @@
     <h4 slot="title">About</h4>
     <div slot="content">
       <section>
-        <h5>Objective</h5>
-        <ol>
-          <li>To bring together the engineers and engineering education professionals of various countries to share current status of engineering education in respective countries and discuss ways to effectively address the need of advancement of engineering education from global perspective.</li>
-          <li>Provide a platform to early stage engineering students to present their works so that they can learn the ways to effectively express their skills in global environment.</li>
-        </ol>
-      </section>
-      <section>
-        <h5>Venue</h5>
-        <p>National Institute of Technology, Kisarazu College</p>
-        <p>2-11-1, Kiyomidaihigashi, Kisarazu, Chiba 292-0041 JAPAN</p>
-        <p>TEL:0438-30-4000</p>
-      </section>
-      <section>
-        <h5>Organizing Committee</h5>
-        <dl>
-          <dt>Chair</dt>
-          <dd>
-            <ul>
-              <li>Prof. Kazuo Maeno, President, National Institute of Technology, Kisarazu College, Japan</li>
-            </ul>
-          </dd>
+        <h5>사진업로드</h5>
+			<div>
+				 <button id="product-gallery-dropzone" v-on:click="$upload.select('product-gallery')" :disabled="$upload.meta('product-gallery').state === 'sending'">
+					  <span v-show="$upload.meta('product-gallery').state === 'sending'">Uploading...</span>
+					  <span v-show="!$upload.meta('product-gallery').state === 'sending'">Select Photos</span>
+				 </button>
 
-          <dt>Co-Chair</dt>
-          <dd>
-            <ul>
-              <li>Prof. Masatoshi Tokita, Dean, International Exchange Center, National Institute of Technology, Kisarazu College, Japan</li>
-            </ul>
-          </dd>
+				 <button v-on:click="$upload.reset('product-gallery')" :disabled="$upload.meta('product-gallery').state === 'sending'">
+					  Clear
+				 </button>
+			</div>
 
-          <dt>General Secretary</dt>
-          <dd>
-            <ul>
-              <li>Dr. Achyut Sapkota, National Institute of Technology, Kisarazu College, Japan</li>
-            </ul>
-          </dd>
+			<div class="progress">
+				 <div class="progress-bar" :style="'width: ' + $upload.meta('product-gallery').percentComplete + '%;'">
+					  {{ $upload.meta('product-gallery').percentComplete }}% Complete
+				 </div>
+			</div>
 
-          <dt>Reviewing Committee</dt>
-          <dd>
-            <ul>
-              <li>Dr. Sanjeeb Prasad Panday, Tribhuvan University, Nepal</li>
-              <li>Dr. Dhundi Raj Pathak, Engineering Study and Research Center, Kathmandu, Nepal</li>
-              <li>Dr. Jiafeng Yao, Nanjing University of Aeronautics and Astronautics, Nanjing, China</li>
-              <li>Dr. Tong Zhao, Xian Institute of Technology, Xian, China</li>
-              <li>Dr. Zhanbolat Shakhmov, Eurasian National University, Astana, Kazakhstan</li>
-              <li>Dr. Phyoe Wai Htun,Mandalay Technological University, Mandalay, Myanmar</li>
-              <li>Dr. Hima Bindu Venigalla, GITAM University, Hyderabad, India</li>
-              <li>Dr. Anil Kumar Kambhampati, Jeju National University, Jeju, South Korea</li>
-              <li>Dr. Trinh Quang Duc, Hanoi University of Science and Technology, Hanoi, Vietnam</li>
-            </ul>
-          </dd>
+			<div v-if="$upload.errors('product-gallery').length" class="text-danger">
+				 {{ $upload.errors('product-gallery')[0].msg }}
+			</div>
 
-          <dt>Local Organization</dt>
-          <dd>
-            <ul>
-              <li>International Exchange Center Committee, National Institute of Technology, Kisarazu College</li>
-            </ul>
-          </dd>
-        </dl>
-      </section>
-      <section>
-        <h5>For further information</h5>
-        <p>Dr. Achyut Sapkota, Department of Information and Computer Engineering, National Institute of Technology, Kisarazu College</p>
-        <p>Email: sapkota@j.kisarazu.ac.jp</p>
+			<div>
+				 <div v-if="!$upload.files('product-gallery').all.length">
+					  No uploads here yet.
+				 </div>
+
+				 <div v-for="file in $upload.files('product-gallery').progress">
+					  <div>
+							{{ file.name }}
+					  </div>
+
+					  <div class="progress">
+							<div class="progress-bar" :style="'width: ' + file.percentComplete + '%;'">
+								 {{ file.percentComplete }}%
+							</div>
+					  </div>
+				 </div>
+
+				 <div v-for="file in $upload.files('product-gallery').queue">
+					  <div>
+							{{ file.name }}
+							<br/>
+							Queued for upload
+					  </div>
+				 </div>
+
+				 <div v-for="file in $upload.files('product-gallery').success">
+					  {{ file.name }}
+					  <br/>
+					  Uploaded successfully.
+				 </div>
+
+				 <div v-for="file in $upload.files('product-gallery').error">
+					  {{ file.name }}
+					  <br/>
+					  {{ file.errors[0].msg }}
+				 </div>
+			</div>
       </section>
     </div>
   </PageArticle>
 </template>
 
 <script>
+import Vue from 'vue';
+import vueUpload from '@websanova/vue-upload';
+
+Vue.use(vueUpload);
+
 export default {
   head: {
     title: 'About'
-  }
+  },
+  created() {
+		 this.$upload.on('product-gallery', {
+			  maxFilesSelect: 20,
+			  dropzoneId: 'product-gallery-dropzone',
+			  multiple: true,
+			  onStart() {
+					 this.$toggle.show('product:media:uploads');
+			  },
+			  onSuccess(res) {
+					this.product.gallery.push(res.data.data);
+			  },
+			  onEnd() {
+					this.$msgbag.success('File upload complete.');
+			  }
+		 });
+	},
+
+	mounted() {
+		 this.$upload.option('product-gallery', {
+			  url: 'products/' + this.product.id + '/gallery'
+		 });
+	},
+
+	beforeDestroy() {
+		 this.$upload.off('product-gallery');
+	},
 }
 </script>
