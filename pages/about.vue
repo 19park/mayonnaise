9d970f471360ccf1a81dcc47974bd8e391b849f4
19park <19park@wholedoc.net>
2018-08-02 12:59:05 +0900

    upload

--- pages/about.vue
+++ pages/about.vue
@@ -5,62 +5,21 @@
       <section>
         <h5>사진업로드</h5>
 			<div>
-				 <button id="product-gallery-dropzone" v-on:click="$upload.select('product-gallery')" :disabled="$upload.meta('product-gallery').state === 'sending'">
-					  <span v-show="$upload.meta('product-gallery').state === 'sending'">Uploading...</span>
-					  <span v-show="!$upload.meta('product-gallery').state === 'sending'">Select Photos</span>
-				 </button>
-
-				 <button v-on:click="$upload.reset('product-gallery')" :disabled="$upload.meta('product-gallery').state === 'sending'">
-					  초기화하기
-				 </button>
-			</div>
-
-			<div class="progress">
-				 <div class="progress-bar" :style="'width: ' + $upload.meta('product-gallery').percentComplete + '%;'">
-					  {{ $upload.meta('product-gallery').percentComplete }}% 완료
-				 </div>
-			</div>
-
-			<div v-if="$upload.errors('product-gallery').length" class="text-danger">
-				 {{ $upload.errors('product-gallery')[0].msg }}
-			</div>
-
-			<div>
-				 <div v-if="!$upload.files('product-gallery').all.length">
-					  No uploads here yet.
-				 </div>
-
-				 <div v-for="file in $upload.files('product-gallery').progress">
-					  <div>
-							{{ file.name }}
-					  </div>
-
-					  <div class="progress">
-							<div class="progress-bar" :style="'width: ' + file.percentComplete + '%;'">
-								 {{ file.percentComplete }}%
-							</div>
-					  </div>
-				 </div>
-
-				 <div v-for="file in $upload.files('product-gallery').queue">
-					  <div>
-							{{ file.name }}
-							<br/>
-							업로드 대기중..
-					  </div>
-				 </div>
-
-				 <div v-for="file in $upload.files('product-gallery').success">
-					  {{ file.name }}
-					  <br/>
-					  업로드 완료.
-				 </div>
-
-				 <div v-for="file in $upload.files('product-gallery').error">
-					  {{ file.name }}
-					  <br/>
-					  {{ file.errors[0].msg }}
-				 </div>
+				 <ul>
+					 <li v-for="file in files">{{file.name}} - Error: {{file.error}}, Success: {{file.success}}</li>
+				  </ul>
+				  <file-upload
+					 ref="upload"
+					 v-model="files"
+					 post-action="/post.method"
+					 put-action="/put.method"
+					 @input-file="inputFile"
+					 @input-filter="inputFilter"
+				  >
+				  Upload file
+				  </file-upload>
+				  <button v-show="!$refs.upload || !$refs.upload.active" @click.prevent="$refs.upload.active = true" type="button">업로드시작</button>
+				  <button v-show="$refs.upload && $refs.upload.active" @click.prevent="$refs.upload.active = false" type="button">업로드중지</button>
 			</div>
       </section>
     </div>
@@ -68,42 +27,59 @@
 </template>
 
 <script>
-import Vue from 'vue';
-import vueUpload from '@websanova/vue-upload';
-
-Vue.use(vueUpload);
+import FileUpload from 'vue-upload-component/src'
 
 export default {
   head: {
     title: 'About'
   },
-  created() {
-		 
+  components: {
+		 FileUpload
 	},
+  data() {
+    return {
+      files: []
+    }
+  },
+  methods: {
+    /**
+     * Has changed
+     * @param  Object|undefined   newFile   Read only
+     * @param  Object|undefined   oldFile   Read only
+     * @return undefined
+     */
+    inputFile: function (newFile, oldFile) {
+      if (newFile && oldFile && !newFile.active && oldFile.active) {
+        // Get response data
+        console.log('response', newFile.response)
+        if (newFile.xhr) {
+          //  Get the response status code
+          console.log('status', newFile.xhr.status)
+        }
+      }
+    },
+    /**
+     * Pretreatment
+     * @param  Object|undefined   newFile   Read and write
+     * @param  Object|undefined   oldFile   Read only
+     * @param  Function           prevent   Prevent changing
+     * @return undefined
+     */
+    inputFilter: function (newFile, oldFile, prevent) {
+      if (newFile && !oldFile) {
+        // Filter non-image file
+        if (!/\.(jpeg|jpe|jpg|gif|png|webp)$/i.test(newFile.name)) {
+          return prevent()
+        }
+      }
 
-	mounted() {
-		this.$upload.on('product-gallery', {
-			  maxFilesSelect: 20,
-			  dropzoneId: 'product-gallery-dropzone',
-			  multiple: true,
-			  onStart() {
-					 this.$toggle.show('product:media:uploads');
-			  },
-			  onSuccess(res) {
-					this.product.gallery.push(res.data.data);
-			  },
-			  onEnd() {
-					this.$msgbag.success('File upload complete.');
-			  }
-		 });
-
-		 this.$upload.option('product-gallery', {
-			  url: '/gallery'
-		 });
-	},
-
-	beforeDestroy() {
-		 this.$upload.off('product-gallery');
-	},
+      // Create a blob field
+      newFile.blob = ''
+      let URL = window.URL || window.webkitURL
+      if (URL && URL.createObjectURL) {
+        newFile.blob = URL.createObjectURL(newFile.file)
+      }
+    }
+	}
 }
 </script>
