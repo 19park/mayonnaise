2e5763eafd20f7bea16bc1c4ac5873bf9c9118f3
19park <19park@wholedoc.net>
2018-08-14 10:29:51 +0900

    gallery stack image load

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -3,12 +3,10 @@
     <h4 slot="title">갤러리</h4>
     <div slot="content">
       <section>
-        <h4>gallery list..</h4>
-
-        <div class="view-area">
-          <stack :column-min-width="320" :gutter-width="8" :gutter-height="8">
+        <div class="view-area" v-images-loaded:on.progress="imageProgress">
+          <stack :column-min-width="200" :gutter-width="8" :gutter-height="8" :monitor-images-loaded="true" @click="selected=element">
             <stack-item v-for="(item, i) in items" :key="i">
-              {{ item.someContent }}
+              <img :src="'https://sempre9mai.cafe24.com/2018/api/mayonnaise/upload/'+item.PATH" />
             </stack-item>
           </stack>
         </div>
@@ -19,32 +17,47 @@
 
 <script>
 import axios from 'axios'
-import { Stack, StackItem } from 'vue-stack-grid';
+import imagesLoaded from 'vue-images-loaded'
+import { Stack, StackItem } from 'vue-stack-grid'
 
 export default {
   head: {
     title: 'Gallery'
   },
-  components: { Stack, StackItem },
   data() {
     return {
       currentPage: 1,
       items: []
     }
   },
+  directives: {
+    imagesLoaded,
+  },
+  components: { Stack, StackItem },
+  methods: {
+    imageProgress(instance, image) {
+      const result = image.isLoaded ? 'loaded' : 'broken';
+      console.log('image is ' + result + ' for ' + image.img.src);
+    },
+    layout() {
+      this.$refs.cpt.layout('masonry');
+    }
+  },
   mounted() {
     let self = this
-    let $url = "http://sempre9mai.cafe24.com/2018/api/mayonnaise/getGalleryList.php";
+    let $url = "https://sempre9mai.cafe24.com/2018/api/mayonnaise/getGalleryList.php";
     axios.get($url, {
       params: {
         page: self.currentPage
       }
     }).then(res => {
-      console.log(res)
-      let getList = res.getList
+      let getList = res.data.LIST
 
       for (let i = 0; i < getList.length; i++) {
+        console.log(getList[i])
+
         self.items.push(getList[i])
+        window.dispatchEvent(new Event('resize'));
       }
     }).catch(err => {
       console.log(err)
@@ -53,3 +66,15 @@ export default {
   }
 }
 </script>
+
+<style>
+.vsg-stack-item {
+  position: relative;
+  transition: left 300ms, top 300ms;
+}
+
+.vsg-stack-item img {
+  display: block;
+  width: 100%;
+}
+</style>
